861d0affb26277331eb38f8857ea1847fb0705f5
Ryuma0724 <s21482804@nucba.ac.jp>
2023-12-09 19:21:47 +0900

    Sixth commit

--- db/schema.rb
+++ db/schema.rb
@@ -60,12 +60,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_03_071613) do
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
     t.string "profile", null: false
     t.string "occupation", null: false
     t.string "position", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
